cfe0b9ba0144cd743d70b38c0a21fb23fe0ffe01
Wene <wene83@gmx.ch>
2022-01-31 20:14:40 +0100

    add terminal class

---
+++ src/terminal.cpp
@@ -0,0 +1,74 @@
+#include "terminal.h"
+
+#define ENTER 13
+#define BACKSPACE 127
+#define ESC 27
+
+Terminal::Terminal(): input_active(false)
+{
+
+}
+
+void Terminal::setup()
+{
+  Serial.begin(115200);
+  Serial.println(F("Starting..."));
+  clear_buffer();
+}
+
+void Terminal::tick()
+{
+  if(!input_active)
+  {
+    return;
+  }
+
+  if(Serial.available())
+  {
+    int in_char = Serial.read();
+    if(BACKSPACE == in_char)
+    {
+      int len = buffer.length();
+      if(0 < len)
+      {
+        buffer = buffer.substring(0, len-1);
+        backspace();
+      }
+    }
+    if(ENTER == in_char)
+    {
+      Serial.println();
+      // TODO: do something with input
+      buffer.clear();
+      input_active = false;
+    }
+    else
+    {
+      buffer += String((char)in_char);
+      Serial.write(in_char);
+    }
+  }
+}
+
+void Terminal::input(tCB_string callback)
+{
+  input_active = true;
+  clear_buffer();
+  input_callback = callback;
+}
+
+void Terminal::backspace()
+{
+  Serial.write(ESC);
+  Serial.print("[D");
+  Serial.write(ESC);
+  Serial.print("[K");
+}
+
+void Terminal::clear_buffer()
+{
+  while(Serial.available())
+  {
+    Serial.read();
+  }
+}
---
+++ src/terminal.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Arduino.h>
+
+typedef void (*tCB_string)(const String &);
+
+class Terminal
+{
+public:
+  Terminal();
+  void setup();
+  void tick();
+  void input(tCB_string callback);
+
+private:
+  void backspace();
+  void clear_buffer();
+
+  String buffer;
+  bool input_active;
+  tCB_string input_callback;
+};
+
