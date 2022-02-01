ce71148821e1d79f9a6b5369c0ec44783f2e5681
Wene <wene83@gmx.ch>
2022-02-01 20:32:37 +0100

    get NTP time

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,40 @@ static void menu_selection(const String &input)
   }
 }
 
+static void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println(F("Failed to obtain time"));
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+}
+
 void setup()
 {
   terminal.setup();
@@ -68,6 +102,7 @@ void setup()
   player.setup();
 
   wifi_setup();
+  configTime(config.get_utc_offset_secs(), config.get_dst_offset_secs(), config.get_ntp_server().c_str());
 
   terminal.input(menu_selection); // TODO: this works only once like this
   scanner_set_terminal_and_config(&terminal, &config);
@@ -78,7 +113,15 @@ void setup()
 
 void loop()
 {
+  static unsigned long last_time = millis();
+  unsigned int now = millis();
   terminal.tick();
+
+  if(now > last_time + 5000)
+  {
+    last_time = now;
+    printLocalTime();
+  }
 
 //  if(Serial.available())
 //  {
