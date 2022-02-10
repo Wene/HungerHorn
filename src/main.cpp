ac6887faa0b4c92b0b25a1bead466ef89a72ea03
Wene <wene83@gmx.ch>
2022-02-10 18:42:11 +0100

    use the time for network setup to boot df-player

--- src/main.cpp
+++ src/main.cpp
@@ -52,9 +52,9 @@ void setup()
 {
   terminal.setup(menu_display);
   config.setup();
-  player.setup();
   network.setup();
   alarmclock.setup();
+  player.setup();
 
   Serial.println(F("Setup done."));
   menu_display();
